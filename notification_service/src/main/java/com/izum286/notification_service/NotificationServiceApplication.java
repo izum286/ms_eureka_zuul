be79d35cfd4d441e6b6111833c0004343bbe46c7
izum <i401623@gmail.com>
2020-04-19 22:18:25 +0300

    config server finished and worked properly

--- notification_service/src/main/java/com/izum286/notification_service/NotificationServiceApplication.java
+++ notification_service/src/main/java/com/izum286/notification_service/NotificationServiceApplication.java
@@ -2,13 +2,15 @@ package com.izum286.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableAsync
-public class NotificationServiceApplication {
+public class NotificationServiceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
